a802344a34107071a4661ee8071fbb57073a59cb
Hanibuchi <hanibuchikouto003@gmail.com>
2025-12-27 13:40:01 +0900

    熊のサイズを適用

--- Assets/Scripts/ResultBear.cs
+++ Assets/Scripts/ResultBear.cs
@@ -7,6 +7,13 @@ public class ResultBear : MonoBehaviour
 
     private void Start()
     {
+        // GameManagerからサイズを適用
+        if (GameManager.Instance != null)
+        {
+            float scale = GameManager.Instance.FinalBearScale;
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
+
         if (_animator == null)
         {
             _animator = GetComponentInChildren<Animator>();
